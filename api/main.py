f40b58cda3cee5229fb81762bf67dace6ede7b23
devzwide <nxumalobukeka66@gmail.com>
2026-03-18 01:07:21 +0200

    Clean Architecture file structure initiated, router initiated as an API endpoint, and database connection modification

--- api/main.py
+++ api/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
-from db.database import create_db_and_tables
+from infrastructure.database import create_db_and_tables
+from presentation.router import router
 
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+app.include_router(router)
